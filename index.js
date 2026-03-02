d048ffa572f28cc970553b29c090600e54c9999c
Luana Leal <98198704+ProfaLuanaLeal@users.noreply.github.com>
2026-03-02 15:27:08 -0400

    Add index.js with initial challenge code

---
+++ index.js
@@ -0,0 +1,9 @@
+// 💡 DESAFIO: Deixe o código limpo!
+const nome = "Caçador"
+let pontos = 50
+
+if (pontos == "50") {
+  console.log("Você iniciou com 50 pontos!");
+}
+
+const itemInutil = "Graveto"; // Esta variável não está sendo usada!
